504dbc496bf9012021dfd3a9fad0442c7327f693
Jhulian11 <jhuliangamer@gmail.com>
2024-02-06 22:43:40 -0300

    Exercise 1 with Vue

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,19 +1,37 @@
-const btn = document.querySelector('button');
-const input = document.querySelector('input');
-const list = document.querySelector('ul');
+// const btn = document.querySelector('button');
+// const input = document.querySelector('input');
+// const list = document.querySelector('ul');
 
-const addGoal = () => {
-    const enteredValue = input.value;
+// const addGoal = () => {
+//     const enteredValue = input.value;
 
-    if(enteredValue.trim() == "")
-        return;
+//     if(enteredValue.trim() == "")
+//         return;
 
-    const listItem = document.createElement('li');
-    
-    listItem.textContent = enteredValue;
-    list.appendChild(listItem);
+//     const listItem = document.createElement('li');
+//     listItem.textContent = enteredValue;
+//     list.appendChild(listItem);
 
-    input.value = "";
-};
+//     input.value = "";
+// };
 
-btn.addEventListener('click', addGoal);
+// btn.addEventListener('click', addGoal);
+
+Vue.createApp({
+    data(){
+        return {
+            goals: [],
+            enteredValue: ""
+        };
+    },
+    methods: {
+        addGoal(){
+            if(this.enteredValue.trim() == "")
+                return;
+
+            this.goals.push(this.enteredValue);
+
+            this.enteredValue = '';
+        }
+    }
+}).mount("#app");
