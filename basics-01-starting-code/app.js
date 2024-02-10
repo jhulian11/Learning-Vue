5784345ef8453ae15a7d6d5dae46e347dbd7ad9c
Jhulian11 <jhuliangamer@gmail.com>
2024-02-10 14:14:58 -0300

    Continuing  learn Vue Basics

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,10 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: "Finish the course and learn Vue",
+            courseGoalA: "Finish the course and learn Vue",
+            courseGoalB: "<h2> Finish the course and master Vue </h2>",
             vueLink: 'https://vuejs.org/'
         };
     },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+
+            if(randomNumber < 0.5)
+                return this.courseGoalA
+            else
+                return this.courseGoalB
+        },
+    }
 });
 
 app.mount("#user-goal");
