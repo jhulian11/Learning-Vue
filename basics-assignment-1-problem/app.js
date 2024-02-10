f631d22ca4ed9a4731c880a521ecc2f4415331b4
Jhulian11 <jhuliangamer@gmail.com>
2024-02-10 14:50:02 -0300

    Doing first assignments

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            myName: "Jhulian Pereira Manhães",
+            myAge: 23,
+            imageLink: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/04/imvaernarhro-1.jpg"
+        };
+    }
+});
+
+app.mount("#assignment");
