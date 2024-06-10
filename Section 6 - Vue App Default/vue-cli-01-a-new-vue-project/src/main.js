3e5abc47d288f16282d107452545f0cdf3381fa1
Jhulian11 <jhuliangamer@gmail.com>
2024-06-10 11:22:23 -0300

    Learning More about VuaApp Default Layout

--- Section 6 - Vue App Default/vue-cli-01-a-new-vue-project/src/main.js
+++ Section 6 - Vue App Default/vue-cli-01-a-new-vue-project/src/main.js
@@ -1,3 +1,9 @@
 import { createApp } from 'vue';
+import App from './App.vue'
+import FriendContatct from "./components/FriendContact.vue"
 
-createApp({}).mount('');
+const app = createApp(App);
+
+app.component('friend-contact', FriendContatct);
+
+app.mount('#app');
