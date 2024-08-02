c17ddc5f1b87eb1de5b546ab8fe413013155ef02
Jhulian11 <jhuliangamer@gmail.com>
2024-08-02 19:00:55 -0300

    Learning more about slots

--- Section 7 - Deep in Components/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ Section 7 - Deep in Components/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,6 +1,39 @@
 <template>
     <div>
         <h2>Manage Goals</h2>
-        <input type="text">
+        <input type="text" ref="goal">
+        <button @click="setGoal" >Set Goal</button>
+        <error-alert v-if="inputIsInvalid"> 
+            <h2>Input Is Invalid</h2>
+            <p>Please enter at leart a few characters...</p>
+            <button @click="confirmError">Okay</button>
+        </error-alert>
     </div>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+
+export default{
+    components: {
+        ErrorAlert
+    },
+    data(){
+        return{
+            inputIsInvalid: false
+        }
+    },
+    methods:{
+        setGoal(){
+            const enteredValue = this.$refs.goal.value;
+            if(enteredValue === '')
+                this.inputIsInvalid = true;
+            else
+                this.inputIsInvalid = false
+        },
+        confirmError(){
+            this.inputIsInvalid = false
+        }
+    }
+}
+</script>
